eabdb6378ab16983fd29e87101efbdd2686d454b
xtawa <zaz203@outlook.com>
2025-07-16 21:14:35 +0800

    update

--- src/config.ts
+++ src/config.ts
@@ -32,11 +32,11 @@ export const siteConfig: SiteConfig = {
 	},
 	favicon: [
 		// Leave this array empty to use the default favicon
-		// {
-		//   src: '/favicon/icon.png',    // Path of the favicon, relative to the /public directory
+	   {
+		 src: 'https://cdn.xtawa.top/a4ml5-9c0h3.png',    // Path of the favicon, relative to the /public directory
 		//   theme: 'light',              // (Optional) Either 'light' or 'dark', set only if you have different favicons for light and dark mode
-		//   sizes: '32x32',              // (Optional) Size of the favicon, set only if you have favicons of different sizes
-		// }
+		 sizes: '32x32',              // (Optional) Size of the favicon, set only if you have favicons of different sizes
+	   }
 	],
 };
 
