927916a41569a8f39bb7c6987da66dfb1d492b6c
xtawa <zaz203@outlook.com>
2025-07-19 10:39:04 +0800

    optmize res load speed

--- src/config.ts
+++ src/config.ts
@@ -13,16 +13,16 @@ export const siteConfig: SiteConfig = {
 	subtitle: "分享技术学习心得",
 	lang: "zh_CN", // 'en', 'zh_CN', 'zh_TW', 'ja', 'ko', 'es', 'th'
 	themeColor: {
-		hue: 360, // Default hue for the theme color, from 0 to 360. e.g. red: 0, teal: 200, cyan: 250, pink: 345
+		hue: 275, // Default hue for the theme color, from 0 to 360. e.g. red: 0, teal: 200, cyan: 250, pink: 345
 		fixed: false, // Hide the theme color picker for visitors
 	},
 	banner: {
-		enable: false,
-		src: "", // Relative to the /src directory. Relative to the /public directory if it starts with '/'
+		enable: true,
+		src: "https://cdn.xtawa.top/bg.4n80abztit.webp", // Relative to the /src directory. Relative to the /public directory if it starts with '/'
 		position: "center", // Equivalent to object-position, only supports 'top', 'center', 'bottom'. 'center' by default
 		credit: {
-			enable: false, // Display the credit text of the banner image
-			text: "夕阳、流星 | @HKcutie", // Credit text to be displayed
+			enable: true, // Display the credit text of the banner image
+			text: "夕阳、流星 | @HKcutie⭐", // Credit text to be displayed
 			url: "https://www.pixiv.net/artworks/115626113", // (Optional) URL link to the original artwork or artist's page
 		},
 	},
